46fc8eff691810748c6dc9fc5abb36de022c8615
lt <logantanous@gmail.com>
2018-05-17 10:38:19 -0700

    small updates

---
+++ README.md
@@ -0,0 +1,23 @@
+# PDX TechCrawl 2018
+
+<p>Currently only a static site that was suggested by The Dyrt as a tool for the TechCrawl 2018 users to see map items</p>
+
+<h2>Tools Used</h2>
+<ul>
+  <li>MapBox</li>
+  <li>Rivets</li>
+</ul>
+
+<h2>Features</h2>
+<ul>
+  <li>Sticky header with body only scrolling to allow for better cross platform experience</li>
+  <li>Sidebar and map that interact with each other</li>
+  <ul>
+    <li>On sidebar item click map is centered on the corresponding map icon</li>
+    <li>On map icon click the item on the sidebar is scrolled to the corresponding item and highlighted</li>
+    <li>On smaller screens the sidebar is removed and on click the details of the clicked company show up below.</li>
+  </ul>
+  <li>Custom styled map using MapBox's styling user interface</li>
+  <li>Ajax handling of data using Rivets</li>
+  <li>Clean non frame-worked CSS</li>
+</ul>  
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+// Need to separate in to separate JS files
+
 mapboxgl.accessToken = 'pk.eyJ1IjoidGhlZHlydCIsImEiOiJjamg4NXE1aGQwZno5MnFwY2lmZHZ5NjJvIn0.O39NoR8r4VqzVRbFZ_yJDQ';
 
 var map = new mapboxgl.Map({
@@ -12,6 +14,7 @@ var map = new mapboxgl.Map({
 // 45.524837,-122.6801553
 window.mapInstance = map;
 
+// Map Nav buttons
 var nav = new mapboxgl.NavigationControl();
 map.addControl(nav, 'top-left');
 
@@ -23,9 +26,11 @@ var geoControl = new mapboxgl.GeolocateControl({
 });
 map.addControl(geoControl, 'top-left');
 
+// adds items on to the map
 window.companies.forEach(function(company, index) {
   var count = index+1;
   var el = document.createElement('div');
+  // marker items
   el.innerHTML = '<div><a href="#'+count+'">'+count+'</a></div>';
   el.className = 'marker';
   el.id = 'mCount'+count;
@@ -41,20 +46,22 @@ window.companies.forEach(function(company, index) {
     // console.log(document.getElementsByClassName('side-bar-items')[index].innerHTML);
     document.getElementById('side-bar2').innerHTML = document.getElementsByClassName('side-bar-items')[index].innerHTML;
 
-
-    map.flyTo({center: this.coordinates});
+    // consider disabling centering of screen on mobile
+    // map.flyTo({center: this.coordinates});
 
     window.companies.forEach(function(company) {
       company.isHighlighted = false;
     });
     this.isHighlighted = true;
     // window.scrollTo(0, 415);
+
     var all = document.getElementsByClassName('marker');
+    // reset gold icons to normal
     for (var i = 0; i < all.length; i++) {
       all[i].style.backgroundImage = 'url(https://email-assets.thedyrt.com/2017/images/number-icon.png)';
     }
+    //set as gold icons when clicked
     el.style.backgroundImage = 'url(https://email-assets.thedyrt.com/2017/images/gold-icon.png)';
-    // el.style.backgroundImage = 'url(https://email-assets.thedyrt.com/2017/images/number-icon.png)';
 
   }.bind(company);
 
@@ -66,9 +73,7 @@ window.companies.forEach(function(company, index) {
     .addTo(map);
 });
 
-// var myEl = document.getElementsByClassName('marker');
-// myEl.onclick = function(event){alert('Hello world');};
-
+// rivets for simple handling of clicks and data modeling
 rivets.formatters.increment = {
   read: function(value) {
     return value + 1;
@@ -82,7 +87,7 @@ rivets.bind(document.body, {
   model: {
     companies: window.companies,
     handleCompanyClick: function(event, data) {
-
+      // sidebar clicks
       console.log('THIS COMPANY WAS CLICKED, ', data.index+1);
       map.flyTo({center: data.model.companies[data.index].coordinates});
       console.log(data.model.companies[data.index].coordinates);
@@ -96,7 +101,7 @@ rivets.bind(document.body, {
         all[i].style.backgroundImage = 'url(https://email-assets.thedyrt.com/2017/images/number-icon.png)';
       }
       document.getElementById('mCount'+item).style.backgroundImage = 'url(https://email-assets.thedyrt.com/2017/images/gold-icon.png)';
-      document.getElementById('side-bar2').innerHTML =  document.getElementsByClassName('side-bar-items')[data.index].innerHTML;      
+      document.getElementById('side-bar2').innerHTML =  document.getElementsByClassName('side-bar-items')[data.index].innerHTML;
     },
   },
 });
